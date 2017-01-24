a299cff4a3bee37484b83d62ebf1e74d994b1dbc
Mike Jarrett <msjarrett@gmail.com>
2017-01-24 13:27:10 -0800

    Adds project and task forms on a basic webpage

--- models.py
+++ models.py
@@ -1,5 +1,42 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User, Group
+from django.forms import ModelForm
 
 # Create your models here.
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=200)
+    groups = models.ForeignKey(Group, null=True, related_name='projects')
+    def __str__(self):
+        return self.project_name
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.tag_name
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=200)
+    project = models.ForeignKey(Project, max_length=200, on_delete=models.CASCADE, null=True, related_name='tasks')
+    description = models.CharField(max_length=2000, null=True, blank=True)
+    user = models.ForeignKey(User, null=True, related_name='tasks')
+    tag = models.ManyToManyField(Tag, related_name='tasks')
+    priority = models.IntegerField(null=True, blank=True)
+    date_created = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return self.task_name
+
+
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ['project_name', 'groups']
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ['task_name','project','description','priority']
--- views.py
+++ views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from todo.models import Project, Task, Tag, ProjectForm, TaskForm
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.decorators import login_required
+
+
+# Helper functions
+def is_group_match(user1, user2):
+    return any(x in user1.groups.all() for x in user2.groups.all())
+
+def add_tags(tag_list, task):
+    for tag in tag_list:
+        try:
+            tag_obj = Tag.objects.get(tag_name=tag)
+        except:
+            tag_obj = Tag(tag_name=tag)
+            tag_obj.save()
+        tag_obj.tasks.add(task)
+
+def create_new_project(project_name,user):
+    p = Project(project_name=project_name, group=user.username)
+    p.save()
+def create_new_task(project, task_name):
+    t = Task(task_name='find new lesions', project=p, description='ID new lesions on SWI and see if they show up on hyperintense on FLAIR', user=user,priority=5)
 
 # Create your views here.
+
+@login_required
+def mainview(request):
+
+    if request.method == 'POST':
+        if 'newproject' in request.POST:
+            form = ProjectForm(request.POST)
+            form.save()
+        elif 'newtask' in request.POST:
+            form = TaskForm(request.POST)
+            form.save()
+
+
+    project_list = [ p for p in Project.objects.all() if p.groups in request.user.groups.all() ]
+    print(project_list)
+
+    context = {'project_list':project_list, 'projectform':ProjectForm(), 'taskform':TaskForm()}
+
+    return render(request, 'todo/mainview.html',context)
